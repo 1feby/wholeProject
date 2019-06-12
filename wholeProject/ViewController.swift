4d49197c4076b35a6b0b87464acf4d20247e5e5d
phoebe <phoebe.ezzat1@gmail.com>
2019-06-12 15:02:25 +0200

    add perfect weather

--- wholeProject/ViewController.swift
+++ wholeProject/ViewController.swift
@@ -7,13 +7,19 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
+import MapKit
+import SwiftyJSON
+import Alamofire
+class ViewController: UIViewController,CLLocationManagerDelegate {
+let locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        googleSearch(searchSent: "مسلسلات رمضان")
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
     }
     // **********************to take photo and save photos ****************************
     func takePhotos (){
@@ -48,6 +54,65 @@ class ViewController: UIViewController {
         let myURL = URL(string: url!)
         UIApplication.shared.open(myURL! , options: [:], completionHandler: nil)
     }
+//***************************   getWeather    *****************************************
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //last value in array will be more accurate
+        let App_id = "1aceb2f3462bcbb96bb892abc52ab2cb"
+        let location = locations[locations.count - 1]
+        if location.horizontalAccuracy > 0 {
+            locationManager.stopUpdatingLocation()
+            print("long = \(location.coordinate.longitude), lat = \(location.coordinate.latitude) ")
+            let latitude = String(location.coordinate.latitude)
+            let longitude = String(location.coordinate.longitude)
+            let params : [String : String ] = [ "lat" : latitude , "lon" : longitude , "appid" : App_id ]
+            getWeather( params: params)
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("yess i fail")
+        //hn7ot alert
+       
+    }
+    func getWeather(params : [String : String]){
+        let weatherURl = "http://api.openweathermap.org/data/2.5/weather"
+        Alamofire.request(weatherURl,method: .get ,parameters: params).responseJSON {
+            response in
+            if response.result.isSuccess{
+                print("success,got weather data")
+                let weatherJSON : JSON = JSON(response.result.value!)
+                print(weatherJSON)
+                self.UpdateWeatherData(json: weatherJSON)
+                
+            }else{
+                // alert connection issues
+                let alert = UIAlertController(title: "connection loss", message: "there is an issue in your connection", preferredStyle: .alert)
+                let OKAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+                alert.addAction(OKAction)
+                self.present(alert, animated: true, completion: nil)
+                print("error,\(response.result.error ?? 0 as! Error)")
+            }
+        }
+    }
+    func UpdateWeatherData(json : JSON){
+        let weatherModel = WeatherDataModel()
+        if let temp = json["main"]["temp"].double{
+            weatherModel.temp = Int(temp - 273.15)
+            weatherModel.city = json["name"].stringValue
+            weatherModel.condition = json["weather"][0]["id"].intValue
+            weatherModel.weatherIcon = weatherModel.updateWeatherIcon(condition: weatherModel.condition)
+            let alert = UIAlertController(title : "the Weather",message : "today , the weather is \(weatherModel.temp) in  \(weatherModel.city)",preferredStyle: .alert)
+            
+            alert.addImage(image: UIImage(named: weatherModel.weatherIcon)!)
+            let OKAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(OKAction)
+            self.present(alert, animated: true, completion: nil)
+        }else{
+            //alert city.text =  locationUNavailable
+            let alert = UIAlertController(title: "Location unavailable", message: "can't reach to your location", preferredStyle: .alert)
+            let OKAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(OKAction)
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
     
-
     }
